55e3d7310781585628c59ff1a812787ce4fe7248
unknown <pateltharsh@gmail.com>
2020-03-30 19:00:13 -0400

    added 3 controllers

--- server.js
+++ server.js
@@ -2,15 +2,17 @@ const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
+const transactions = require('./routes/transactions');
+const connectDB = require('./config/db');
 
 dotenv.config({ path: './config/config.env' });
 
-const app = express();
+connectDB();
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
-});
+const app = express();
+app.use(express.json());
+
+app.use('/api/v1/transactions', transactions);
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, console.log(`Server running on in ${process.env.NODE_ENV} mode on port ${PORT}.`.yellow.bold));
